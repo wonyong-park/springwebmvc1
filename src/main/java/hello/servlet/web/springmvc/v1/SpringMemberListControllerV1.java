6500911ccfaea3b033fb5d2f84210814dfa38450
wonyong-park <wanyong0919@naver.com>
2021-10-12 17:08:32 +0900

    2021-10-12 @Controller 추가

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -3,12 +3,14 @@ package hello.servlet.web.springmvc.v1;
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
 
+@Controller
 public class SpringMemberListControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
